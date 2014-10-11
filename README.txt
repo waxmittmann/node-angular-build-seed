0b653c385ea94746c967517ea2e782012120a440
Maximilian Wittmann <damxam@gmail.com>
2014-10-11 15:04:59 +1100

    Add testing for angular controller using karma

---
+++ README.txt
@@ -0,0 +1,6 @@
+A simple seed project using the following stack:
+
+-web server: node
+-packaging/dependency management: npm, gulp, bower
+-style: sass
+-client-side MVC: angular
--- public/js/SimpleController.js
+++ public/js/SimpleController.js
@@ -3,5 +3,9 @@ var app = angular.module('TemplateApp', []);
 app.controller('SimpleController', [
   '$scope', function($scope) {
     $scope.title = "Hello Angular";
+    $scope.name = "Max";
+    $scope.sayHello = function() {
+      $scope.greeting = "Hello " + $scope.name;
+    }
   }
 ]);
