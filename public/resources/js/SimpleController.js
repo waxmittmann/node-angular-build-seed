310c1f7000ffe48d21b226c2141c5c15406039b2
Maximilian Wittmann <damxam@gmail.com>
2014-10-15 21:50:10 +1100

    Change directory structure so static html is served from / and other stuff from /resources.
    Added an example angular directive.

--- public/resources/js/SimpleController.js
+++ public/resources/js/SimpleController.js
@@ -9,3 +9,11 @@ app.controller('SimpleController', [
     }
   }
 ]);
+
+app.directive('helloWorld', function() {
+  return {
+      restrict: 'AE',
+      replace: 'true',
+      template: '<h3>Hello World!!</h3>'
+  };
+});
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public/static'));
+app.use('/resources', express.static(__dirname + '/public/resources'));
 //app.use(express.static(__dirname + '/public'));
 
 app.listen(8000);
